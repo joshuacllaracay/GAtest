07ea06dfbb573e3dba30250ad4811a580b76160b
Joshua Claracay <jc.claracay@gmail.com>
2024-01-23 10:50:55 +0800

    click me

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import ReactGA from 'react-ga4'
+import ReactGA from 'react-ga4';
 import { useEffect } from 'react';
 
 function MainPage() {
@@ -16,8 +16,15 @@ function MainPage() {
     });
   };
 
+  const handleButtonClick = (buttonName: string) => {
+    ReactGA.event({
+      category: 'Button Click',
+      action: `Clicked ${buttonName}`,
+    });
+  };
+
   useEffect(() => {
-    ReactGA.send("page_view_Main-page" + window.location.search);
+    ReactGA.send('page_view_Main-page' + window.location.search);
   }, []);
 
   return (
@@ -32,8 +39,24 @@ function MainPage() {
       <h1>Google Analytics Test environment</h1>
       <div className="card my-4 p-4 bg-white rounded-md shadow-md max-w-md">
         <button
-          onClick={handleLoginClick}
+          onClick={() => {
+            handleButtonClick('Click me 1');
+          }}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Click me 1
+        </button>
+        <button
+          onClick={() => {
+            handleButtonClick('Click me 2');
+          }}
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2"
+        >
+          Click me 2
+        </button>
+        <button
+          onClick={handleLoginClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
         >
           Click to Log In
         </button>
